ef81df3944671f436a7b40b9c15d87f0b239a82f
David <davidsilva4666@gmail.com>
2023-04-01 10:30:50 +0200

    Funcionalidad para finalizar partido y quitar partido de la tabla de resultados

--- src/main/java/tdd/models/ScoreBoard.java
+++ src/main/java/tdd/models/ScoreBoard.java
@@ -23,16 +23,37 @@ public class ScoreBoard {
 
 	public void gameAdd(Game partido) {
 		resultados.add(partido);
-		
+
 	}
 
 	public void partidosEnJuego() {
 		System.out.println("******RESULTADOS******");
 		for (Game game : resultados) {
-			System.out.println(game.getTeam1()+" "+game.getTeam1Score()+" - "+game.getTeam2Score()+" "+game.getTeam2());
+			System.out.println(game.getTeam1() + " " + game.getTeam1Score() + " - " + game.getTeam2Score() + " "
+					+ game.getTeam2());
+		}
+
+	}
+
+	public Game obtenerPrimerPartido() {
+		Game partido = null;
+		if (resultados.size() > 0) {
+			partido = resultados.get(0);
+		}
+		return partido;
+	}
+
+	public void quitarFinalizados() {
+		for (Game game : resultados) {
+			if(game.getEstado()==1) {
+				resultados.remove(game);
+			}
 		}
 		
 	}
-	
-	
+
+	public int numeroPartidos() {
+		return resultados.size();
+	}
+
 }
--- src/test/java/sportradar/tdd/ScoreBoardTest.java
+++ src/test/java/sportradar/tdd/ScoreBoardTest.java
@@ -18,9 +18,11 @@ public class ScoreBoardTest {
 	
 	ScoreBoard tablaResultados = new ScoreBoard();
 	
-	//Vamos a comprobar que tras iniciar el partido el resultado es de 0-0
+	//Comprobar que tras iniciar el partido el resultado es de 0-0
 	@Test
 	public void iniciarPartido() {
+		
+		System.out.println("Test iniciarPartido()");
 		//Given
 		Game partido = new Game("Brasil", "Argentina");
 		
@@ -30,7 +32,33 @@ public class ScoreBoardTest {
 		
 		assertEquals("0-0", partido.gameScore());
 		tablaResultados.partidosEnJuego();
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
 	}
+	
+	//Comprobar que tras finalizar el partido, el partido desaparece de la tabla de resultados
+		@Test
+		public void finalizarPartido() {
+			System.out.println("Test finalizarPartido()");
+			//Given
+			Game partido = null;
+			
+			//When
+			partido = tablaResultados.obtenerPrimerPartido();
+			if(partido!=null) {
+				partido.setEstado(1);
+			}
+			
+			tablaResultados.quitarFinalizados();
+			
+			//Then
+			assertEquals(0, tablaResultados.numeroPartidos());
+			tablaResultados.partidosEnJuego();
+			System.out.println("");
+			System.out.println("");
+			System.out.println("");
+		}
 	
 
 }
